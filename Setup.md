0302d85bd565fc09cac14fb3162566b14bd93abf
John Is A Nerd <admin@dexterindustries.com>
2017-10-31 13:04:00 +0400

    Initial Commit
    Getting the bot up and running.  There are some flaws.

---
+++ Setup.md
@@ -0,0 +1,5 @@
+Enable ownership of files with `sudo chown -R pi:pi ~/redditbot/`
+Install PRAW: `sudo pip install praw`
+
+
+Create an App:  https://www.reddit.com/prefs/apps/  
---
+++ redditbot.py
@@ -0,0 +1,104 @@
+# redditbot
+# An Experiment in Karma
+# Add "Nice work!" to every subreddit entry.
+# Link to API information:  https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
+
+import re
+import os
+import praw
+import time
+import random
+
+debug_api_on = True
+
+def debug_api(string_in):
+    if debug_api_on:
+        print("Debug API: # " + string_in)
+
+reddit = praw.Reddit('bot1', user_agent='Social_Bot_1.0')
+subreddit = reddit.subreddit("RASPBERRY_PI_PROJECTS")
+
+print("#######")
+print(reddit.user.me())
+
+# Vague replies to questions.
+question_replies = [
+                    "Wish I could help!  Sounds like a cool project!",
+                    "Great project idea.",
+                    "This is super cool. Wish I could help!",
+                    "Good luck on this one!"
+                    ]
+
+project_replies = [
+                    "Cool project, nice work!",
+                    "This project is fabulous.",
+                    "Legendary project!",
+                    "This is very useful information.  Thanks!",
+                    "Great project!"
+                    ]
+
+def reply_on_project(text):
+    # If it's a question, urge them along!
+    if(text.find("?") > 0):
+        return question_replies[random.randint(0,len(question_replies)-1)]
+    # If it's a project or link, compliment the project.
+    return project_replies[random.randint(0,len(project_replies)-1)]
+
+def remove_non_ascii_1(text):
+    return ''.join([i if ord(i) < 128 else ' ' for i in text])
+
+with open("posts_replied_to.txt", "r") as f:
+    posts_replied_to = f.read()
+    posts_replied_to = posts_replied_to.split("\n")
+    posts_replied_to = list(filter(None, posts_replied_to))
+
+print posts_replied_to
+
+while True:
+    # Get the new subreddits.
+    # Check which subreddits have replies.
+    # Reply to any subreddits that don't have replies.
+    for submission in subreddit.new(limit=25):
+        if submission.id not in posts_replied_to:
+            if debug_api_on:
+                print("Title: ", submission.title)
+                print("Text: ", submission.selftext)
+                # print("Score: ", submission.score)
+                print("ID: ", submission.id)
+                print("---------------------------------\n")
+            # Now do this until we get a positive submission.
+            not_submitted = True    # Start with it not submitted
+            while not_submitted:
+                try:
+                    submit_text = remove_non_ascii_1(submission.selftext)
+                    submit_text = reply_on_project(submit_text)
+                    submission.reply(submit_text)
+                    with open("posts_replied_to.txt", "a") as f:
+                        f.write(submission.id + "\n")
+                    not_submitted = False
+                except praw.exceptions.APIException as e:
+                    debug_api("Exception: " + str(e))
+                    if(str(e).find("minute") > 0):
+                        position = str(e).find("minutes")
+                        sleep_minutes = int(filter(str.isdigit, str(e)))
+                        print("Sleeping minutes: " + str(sleep_minutes))
+                        time.sleep((sleep_minutes*60)+5)
+                    elif(str(e).find("seconds") > 0):
+                        sleep_seconds_int = int(filter(str.isdigit, str(e)))
+                        print("Sleeping seconds: " + str(sleep_seconds_int))
+                        time.sleep(sleep_seconds_int)
+                    else:
+                        debug_api("Sleeping 10 seconds")
+                        time.sleep(10)
+                except Exception as e:
+                    print("Unknown exception:")
+                    print(str(e))
+                else:
+                    debug_api("Exception . . . not sure what happened.")
+        else:
+            print("Found a Post:  Already submitted to this one.")
+
+    print("Checked.  Sleeping for 10 minutes.")
+    time.sleep(10*60)
+#if __name__ == '__main__':
+#    main()
